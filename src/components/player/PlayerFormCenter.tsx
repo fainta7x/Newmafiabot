5b7a6c7cb87a34f1706b96061085994dda19cbf2
fainta7x <fainta7x@gmail.com>
2026-08-13 07:43:29 +0300

    chore(typecheck): clean form center import

--- src/components/player/PlayerFormCenter.tsx
+++ src/components/player/PlayerFormCenter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import PlayerStoriesPanel from './PlayerStoriesPanel.tsx';
 import PlayerProgressionPanel from './PlayerProgressionPanel.tsx';
 

09e97d1fe13d8ac298973278a7c1856b445d612a
fainta7x <fainta7x@gmail.com>
2026-08-13 07:44:53 +0300

    chore(typecheck): clean club section import

--- src/components/player/PlayerClubSection.tsx
+++ src/components/player/PlayerClubSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import PlayerProgressionPanel from './PlayerProgressionPanel.tsx';
 import PlayerStoriesPanel from './PlayerStoriesPanel.tsx';
 

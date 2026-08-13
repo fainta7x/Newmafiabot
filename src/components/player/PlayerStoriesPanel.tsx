97f8d8c64e1af83ddb9d8fef4047739f4ec582f7
fainta7x <fainta7x@gmail.com>
2026-08-13 08:05:35 +0300

    chore(typecheck): clean stories import

--- src/components/player/PlayerStoriesPanel.tsx
+++ src/components/player/PlayerStoriesPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import type { PlayerGameDetailData } from './PlayerGameDetail.tsx';
 import EveningVotingPanel from './EveningVotingPanel.tsx';
 

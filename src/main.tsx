6ef4b113ec7f53df06a524f19da1e9b3b2342922
fainta7x <fainta7x@gmail.com>
2026-08-20 10:19:59 +0300

    design: load death protocol cabinet layer

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import "./components/crm/liveGameSeatCabinet.css";
 import "./components/crm/liveGameActionPriority.css";
 import "./components/crm/liveGameNightReadability.css";
 import "./components/crm/liveGameRecoveryPolish.css";
+import "./components/crm/liveGameDeathProtocolCabinet.css";
 import "./components/crm/liveGameTelegram.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(

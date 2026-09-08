ee27951fd18099debec3374df79eceefbf0ea7e9
fainta7x <fainta7x@gmail.com>
2026-09-08 17:54:44 +0300

    Apply CRM primary title ownership globally

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import JudgeGameMusicController from "./components/JudgeGameMusicController.tsx"
 import ScopedEveningDeathProtocolBridge from "./components/crm/ScopedEveningDeathProtocolBridge.tsx";
 import LiveGameResumeBridge from "./components/crm/LiveGameResumeBridge.tsx";
 import AppErrorBoundary from "./components/ui/AppErrorBoundary.tsx";
+import { initializeCrmPrimaryTitleOwnership } from "./lib/crmPrimaryTitleOwnership.ts";
 import { initializeTelegramWebAppViewport } from "./lib/telegramWebAppViewport.ts";
 import "./index.css";
 import "./styles/design-system.css";
@@ -23,6 +24,7 @@ import "./components/crm/liveGameMobileGeometryV6.css";
 import "./components/public/liveBroadcastCompact.css";
 
 initializeTelegramWebAppViewport();
+initializeCrmPrimaryTitleOwnership();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

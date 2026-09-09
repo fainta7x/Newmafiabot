439845635cbcd9fff4cc3174835157948a43bf52
fainta7x <fainta7x@gmail.com>
2026-09-09 09:18:57 +0300

    Mount player notification preference routes

--- src/server/routes/playerSelfRoutes.ts
+++ src/server/routes/playerSelfRoutes.ts
@@ -4,6 +4,7 @@ import playerEventCalendarRoutes from './playerEventCalendarRoutes.ts';
 import playerProfileIntegrityRoutes from './playerProfileIntegrityRoutes.ts';
 import playerProfilePrivacyRoutes from './playerProfilePrivacyRoutes.ts';
 import premiumPlayerProfileRoutes from './premiumPlayerProfileRoutes.ts';
+import playerNotificationPreferenceRoutes from './playerNotificationPreferenceRoutes.ts';
 
 const router = Router();
 router.use(playerSelfCoreRoutes);
@@ -11,5 +12,6 @@ router.use(playerEventCalendarRoutes);
 router.use(playerProfileIntegrityRoutes);
 router.use(playerProfilePrivacyRoutes);
 router.use(premiumPlayerProfileRoutes);
+router.use(playerNotificationPreferenceRoutes);
 
 export default router;
